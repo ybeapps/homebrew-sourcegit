324c67557977dd5d12d677655a7f21de0df5baa3
Anka <runner@Mac-1739859473203.local>
2025-02-18 06:27:56 +0000

    sourcegit 2025.05

--- Casks/sourcegit.rb
+++ Casks/sourcegit.rb
@@ -1,9 +1,9 @@
 cask "sourcegit" do
   arch arm: "arm64", intel: "x64"
 
-  version "2025.04"
-  sha256 arm:   "a37c5f73577962c551c1a0ff95ba257e4cf8db52affd58ed940ff7be3d242633",
-         intel: "57bad5b2c7a88fc9e960654264a4cd9b8eaa4050fda9b57146528bdae6f27e37"
+  version "2025.05"
+  sha256 arm:   "cdb5a4afde9e65850f904b9cbfdc93d266e484a32072cd8367060c8b2df94559",
+         intel: "7cb5a5e1e2c0d420116d87e1357c63767573143bb706f8ce46ddad6f1fc79e67"
 
   url "https://github.com/sourcegit-scm/sourcegit/releases/download/v#{version}/sourcegit_#{version}.osx-#{arch}.zip",
       verified: "github.com/sourcegit-scm/sourcegit/"
