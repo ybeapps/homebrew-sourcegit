3b9cf79a642a27a6f8a63fd96b860aa7b7ed0605
Anka <runner@Mac-1751348835097.local>
2025-07-01 05:54:49 +0000

    Update SourceGit to version 2025.24

--- Casks/sourcegit.rb
+++ Casks/sourcegit.rb
@@ -1,9 +1,9 @@
 cask "sourcegit" do
   arch arm: "arm64", intel: "x64"
 
-  version "2025.21"
-  sha256 arm:   "f9add67f6630ebd6d4c7524a8f1d37e4e61a39fcd7d4449e60270a9cca605400",
-         intel: "dc4301574b4558e311588bbed1486721e96ab1bb226fcad810b9391e43fc696b"
+  version "2025.24"
+  sha256 arm:   "7e54166542f30991fdf1b3d7906261088a044912e296bd4f005913a57f126de4",
+         intel: "d1792db698700d1839678a8792a944e004ffc9a7d8ace141109419606221bd7b"
 
   url "https://github.com/sourcegit-scm/sourcegit/releases/download/v#{version}/sourcegit_#{version}.osx-#{arch}.zip",
       verified: "github.com/sourcegit-scm/sourcegit/"
